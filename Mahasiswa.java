498c7467b25de8e7a122b0058b4b37320f9f7ddc
yogialfin23 <43507570+yogialfin23@users.noreply.github.com>
2018-10-09 00:30:58 +0700

    Add files via upload

---
+++ Mahasiswa.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Mahasiswa {
+    public String nama;
+    public String nim;
+
+    public void perkenalanDiri(String nim, String nama) {
+        System.out.println("Hello Everyone");
+        System.out.println("My Nim is " + nim);
+        System.out.println("My Name is " + nama + "\n");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+/**
+ * Yogie Alfin Salim
+ * 10117100
+ * IF - 3
+ * Deskripsi : Program memperkenalkan diri mahasiswa dengan nama dan nim
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Mahasiswa mhs = new Mahasiswa();
+        mhs.perkenalanDiri("10117100", "Yogie Alfin Salim");
+        mhs.perkenalanDiri("10117112", "Faksi Kilat");
+        mhs.perkenalanDiri("10117116", "Muhammad Fitrayana");
+
+    }
+
+}
+
